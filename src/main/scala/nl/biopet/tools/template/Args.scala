4d7408898a30af527cf6a51bf134cd558b2ef35e
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-04 09:57:55 +0200

    Added fastqsplitter code

---
+++ src/main/scala/nl/biopet/tools/template/Args.scala
@@ -0,0 +1,5 @@
+package nl.biopet.tools.template
+
+import java.io.File
+
+case class Args(inputFile: File = null)
--- src/main/scala/nl/biopet/tools/template/Main.scala
+++ src/main/scala/nl/biopet/tools/template/Main.scala
@@ -4,6 +4,14 @@ import nl.biopet.utils.tool.ToolCommand
 
 object Main extends ToolCommand {
   def main(args: Array[String]): Unit = {
+    val parser = new ArgsParser(this.getClass.getPackage.getName.split(".").last)
+    val cmdArgs =
+      parser.parse(args, Args()).getOrElse(throw new IllegalArgumentException)
 
+    logger.info("Start")
+
+    //TODO: Execute code
+
+    logger.info("Done")
   }
 }

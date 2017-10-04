a808583115cfde34b4ea6d359ac3dba156c2c4ce
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-04 10:02:33 +0200

    formatter

--- src/main/scala/nl/biopet/tools/template/Main.scala
+++ src/main/scala/nl/biopet/tools/template/Main.scala
@@ -4,7 +4,8 @@ import nl.biopet.utils.tool.ToolCommand
 
 object Main extends ToolCommand {
   def main(args: Array[String]): Unit = {
-    val parser = new ArgsParser(this.getClass.getPackage.getName.split(".").last)
+    val parser = new ArgsParser(
+      this.getClass.getPackage.getName.split(".").last)
     val cmdArgs =
       parser.parse(args, Args()).getOrElse(throw new IllegalArgumentException)
 

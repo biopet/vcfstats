3eccaffba25939050d7b8d3cd60106a51b7fd629
pjvan_thof <pjrvanthof@gmail.com>
2017-10-04 11:53:07 +0200

    Adding tests

--- src/main/scala/nl/biopet/tools/template/ToolTemplate.scala
+++ src/main/scala/nl/biopet/tools/template/ToolTemplate.scala
@@ -2,10 +2,9 @@ package nl.biopet.tools.template
 
 import nl.biopet.utils.tool.ToolCommand
 
-object Main extends ToolCommand {
+object ToolTemplate extends ToolCommand {
   def main(args: Array[String]): Unit = {
-    val parser = new ArgsParser(
-      this.getClass.getPackage.getName.split(".").last)
+    val parser = new ArgsParser(toolName)
     val cmdArgs =
       parser.parse(args, Args()).getOrElse(throw new IllegalArgumentException)
 

dd4698618cfd82a5c85313037cd4336c4c3f7234
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-24 19:32:16 +0100

    Move tests

--- src/test/scala/nl/biopet/tools/vcfstats/VcfStatsSparkTest.scala
+++ src/test/scala/nl/biopet/tools/vcfstats/VcfStatsSparkTest.scala
@@ -122,20 +122,4 @@ class VcfStatsSparkTest extends BiopetTest {
     noException should be thrownBy VcfStatsSpark.main(
       Array("-I", vcf, "-R", ref, "-o", tmp.toAbsolutePath.toString))
   }
-
-  @Test
-  def testSortAnyAny(): Unit = {
-    //stub
-    val one: Any = 1
-    val two: Any = 2
-    val text: Any = "hello"
-    val text2: Any = "goodbye"
-
-    sortAnyAny(one, two) shouldBe true
-    sortAnyAny(two, one) shouldBe false
-    sortAnyAny(text, text2) shouldBe false
-    sortAnyAny(text2, text) shouldBe true
-    sortAnyAny(one, text) shouldBe true
-    sortAnyAny(text, one) shouldBe false
-  }
 }
--- src/test/scala/nl/biopet/tools/vcfstats/VcfStatsTest.scala
+++ src/test/scala/nl/biopet/tools/vcfstats/VcfStatsTest.scala
@@ -125,20 +125,4 @@ class VcfStatsTest extends BiopetTest {
     noException should be thrownBy VcfStats.main(
       Array("-I", vcf, "-R", ref, "-o", tmp.toAbsolutePath.toString))
   }
-
-  @Test
-  def testSortAnyAny(): Unit = {
-    //stub
-    val one: Any = 1
-    val two: Any = 2
-    val text: Any = "hello"
-    val text2: Any = "goodbye"
-
-    sortAnyAny(one, two) shouldBe true
-    sortAnyAny(two, one) shouldBe false
-    sortAnyAny(text, text2) shouldBe false
-    sortAnyAny(text2, text) shouldBe true
-    sortAnyAny(one, text) shouldBe true
-    sortAnyAny(text, one) shouldBe false
-  }
 }

6fbf5a074b5bc4569f0b8e730073ce7038cb71e5
pjvan_thof <pjrvanthof@gmail.com>
2018-02-15 15:50:13 +0100

    auto format

--- build.sbt
+++ build.sbt
@@ -10,8 +10,14 @@ biopetIsTool := true
 
 mainClass in assembly := Some("nl.biopet.tools.vcfstats.VcfStats")
 
-developers += Developer(id="ffinfo", name="Peter van 't Hof", email="pjrvanthof@gmail.com", url=url("https://github.com/ffinfo"))
-developers += Developer(id="rhpvorderman", name="Ruben Vorderman", email="r.h.p.vorderman@lumc.nl", url=url("https://github.com/rhpvorderman"))
+developers += Developer(id = "ffinfo",
+                        name = "Peter van 't Hof",
+                        email = "pjrvanthof@gmail.com",
+                        url = url("https://github.com/ffinfo"))
+developers += Developer(id = "rhpvorderman",
+                        name = "Ruben Vorderman",
+                        email = "r.h.p.vorderman@lumc.nl",
+                        url = url("https://github.com/rhpvorderman"))
 
 scalaVersion := "2.11.11"
 
@@ -19,10 +25,10 @@ dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-core" % "2.8.7"
 dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-databind" % "2.8.7"
 dependencyOverrides += "com.fasterxml.jackson.module" % "jackson-module-scala_2.11" % "2.8.7"
 
-libraryDependencies += "com.github.biopet" %% "tool-utils" % "0.3-SNAPSHOT" changing()
-libraryDependencies += "com.github.biopet" %% "spark-utils" % "0.3-SNAPSHOT" changing()
+libraryDependencies += "com.github.biopet" %% "tool-utils" % "0.3-SNAPSHOT" changing ()
+libraryDependencies += "com.github.biopet" %% "spark-utils" % "0.3-SNAPSHOT" changing ()
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.1" % Provided
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.1" % Provided
 
-libraryDependencies += "com.github.biopet" %% "tool-test-utils" % "0.2-SNAPSHOT" % Test changing()
+libraryDependencies += "com.github.biopet" %% "tool-test-utils" % "0.2-SNAPSHOT" % Test changing ()
--- src/test/scala/nl/biopet/tools/vcfstats/VcfStatsTest.scala
+++ src/test/scala/nl/biopet/tools/vcfstats/VcfStatsTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2014 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.vcfstats
 
 import java.io.File
@@ -88,7 +109,8 @@ class VcfStatsTest extends ToolTest[Args] {
     val ref = resourcePath("/fake_chrQ.fa")
 
     intercept[IllegalArgumentException] {
-      executable.main(Array("-I", vcf, "-R", ref, "-o", tmp.toAbsolutePath.toString))
+      executable.main(
+        Array("-I", vcf, "-R", ref, "-o", tmp.toAbsolutePath.toString))
     }.getMessage shouldBe s"requirement failed: ${tmp.toAbsolutePath.toString} does not exist"
   }
 
@@ -110,7 +132,14 @@ class VcfStatsTest extends ToolTest[Args] {
     val ref = resourcePath("/fake_chrQ.fa")
 
     noException should be thrownBy executable.main(
-      Array("-I", vcf, "-R", ref, "-o", tmp.toAbsolutePath.toString, "--binSize", "1000"))
+      Array("-I",
+            vcf,
+            "-R",
+            ref,
+            "-o",
+            tmp.toAbsolutePath.toString,
+            "--binSize",
+            "1000"))
     testOutput(tmp.toFile, contigs = "chrQ" :: Nil)
   }
 
@@ -132,9 +161,22 @@ class VcfStatsTest extends ToolTest[Args] {
     val ref = resourcePath("/fake_chrQ.fa")
 
     noException should be thrownBy executable.main(
-      Array("-I", vcf, "-R", ref, "-o", tmp.toAbsolutePath.toString,
-        "--skipGeneral", "--skipGenotype", "--skipSampleCompare", "--skipSampleDistributions"))
-    testOutput(tmp.toFile, contigs = "chrQ" :: Nil, skipGeneral = true, skipGenotype = true, skipSampleDistributions = true, skipSampleCompare = true)
+      Array("-I",
+            vcf,
+            "-R",
+            ref,
+            "-o",
+            tmp.toAbsolutePath.toString,
+            "--skipGeneral",
+            "--skipGenotype",
+            "--skipSampleCompare",
+            "--skipSampleDistributions"))
+    testOutput(tmp.toFile,
+               contigs = "chrQ" :: Nil,
+               skipGeneral = true,
+               skipGenotype = true,
+               skipSampleDistributions = true,
+               skipSampleCompare = true)
   }
 
   @Test(dataProvider = "executables")
@@ -144,11 +186,16 @@ class VcfStatsTest extends ToolTest[Args] {
     val ref = resourcePath("/fake_chrQ.fa")
 
     noException should be thrownBy executable.main(
-      Array("-I", vcf, "-R", ref, "-o", tmp.toAbsolutePath.toString, "--sparkMaster", "local[1]"))
+      Array("-I",
+            vcf,
+            "-R",
+            ref,
+            "-o",
+            tmp.toAbsolutePath.toString,
+            "--sparkMaster",
+            "local[1]"))
     testOutput(tmp.toFile, contigs = "chrQ" :: Nil)
   }
-
-
   @Test(dataProvider = "executables")
   def testEmptyVcf(executable: ToolCommand[Args]): Unit = {
     val tmp = Files.createTempDirectory("vcfStats")
@@ -167,7 +214,14 @@ class VcfStatsTest extends ToolTest[Args] {
     val ref = resourcePath("/fake_chrQ.fa")
 
     noException should be thrownBy executable.main(
-      Array("-I", vcf, "-R", ref, "-o", tmp.getAbsolutePath, "--infoTag", "DP:All"))
+      Array("-I",
+            vcf,
+            "-R",
+            ref,
+            "-o",
+            tmp.getAbsolutePath,
+            "--infoTag",
+            "DP:All"))
     testOutput(tmp, contigs = "chrQ" :: Nil)
 
     val tsv = new File(tmp, "info.DP_All.tsv")
@@ -188,7 +242,14 @@ class VcfStatsTest extends ToolTest[Args] {
     val ref = resourcePath("/fake_chrQ.fa")
 
     noException should be thrownBy executable.main(
-      Array("-I", vcf, "-R", ref, "-o", tmp.getAbsolutePath, "--genotypeTag", "DP:All"))
+      Array("-I",
+            vcf,
+            "-R",
+            ref,
+            "-o",
+            tmp.getAbsolutePath,
+            "--genotypeTag",
+            "DP:All"))
     testOutput(tmp, contigs = "chrQ" :: Nil)
 
     val tsv = new File(tmp, "genotype.DP_All.tsv")
